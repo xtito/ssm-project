1c0e82ca90b117c8f41346bf731ced58479cf45e
xtito <7924023@qq.com>
2016-06-06 20:42:19 +0800

    add

--- ssm-service/src/main/java/com/ssm/service/UserService.java
+++ ssm-service/src/main/java/com/ssm/service/UserService.java
@@ -1,5 +1,10 @@
 package com.ssm.service;
 
+import com.ssm.bean.User;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by [Zy]
  * 2016/6/5 21:52
@@ -7,5 +12,14 @@ package com.ssm.service;
 
 public interface UserService {
 
+    void saveUser(User user);
+
+    void deleteUser(User user);
+
+    void updateUser(User user);
+
+    List<User> getUserList(Map<String, Object> param);
+
+    User getUserById(String uuid);
 
 }
